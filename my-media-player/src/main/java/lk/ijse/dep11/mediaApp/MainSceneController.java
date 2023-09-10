906d02325be8a45bee478e1757f039b2a792bee4
Sahan Lakmal <sahanloser@gmail.com>
2023-09-11 01:31:54 +0530

    Implement all the buttons and sliders

--- my-media-player/src/main/java/lk/ijse/dep11/mediaApp/MainSceneController.java
+++ my-media-player/src/main/java/lk/ijse/dep11/mediaApp/MainSceneController.java
@@ -1,16 +1,24 @@
 package lk.ijse.dep11.mediaApp;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
+import javafx.util.Duration;
 
+import javafx.scene.input.MouseEvent;
 import java.io.File;
 
 public class MainSceneController {
@@ -18,14 +26,30 @@ public class MainSceneController {
     public Button btnPlay;
     public Button btnPause;
     public Button btnStop;
-    public Button btnBackward;
-    public Button btnForward;
     public Button btnBack10s;
     public Button btnFront10s;
     public MediaView mediaView;
     public BorderPane borderPane;
+    public Button btnSlowRate;
+    public Button btnFastForward;
+    public Slider pbProgress;
+    public Slider volumeSlider;
 
     private MediaPlayer mediaPlayer;
+    private Timeline timeline;
+
+    public void initialize(){
+        // Initialize the volume slider
+        volumeSlider.setValue(50.0); // Set the initial volume value (e.g., 50%)
+
+        // Add a listener to the volume slider to adjust the media player's volume
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.setVolume(newValue.doubleValue() / 100.0); // Adjust volume from 0.0 to 1.0
+            }
+        });
+
+    }
 
     public void btnChooseFileOnAction(ActionEvent e) {
         FileChooser fileChooser = new FileChooser();
@@ -55,31 +79,104 @@ public class MainSceneController {
             widthProp.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
             heightProp.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
 
-            mediaPlayer.play();
-        }
+            mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+                @Override
+                public void changed(ObservableValue<? extends Duration> observableValue, Duration oldValue, Duration newValue) {
+                    pbProgress.setValue(newValue.toSeconds());
+                }
+            });
 
+            // Add an EventHandler to the Slider to handle user dragging
+            pbProgress.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    mediaPlayer.seek(Duration.seconds(pbProgress.getValue()));
+
+                }
+            });
+
+            pbProgress.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    mediaPlayer.seek(Duration.seconds(pbProgress.getValue()));
+
+                }
+            });
+
+            mediaPlayer.setOnReady(new Runnable() {
+                @Override
+                public void run() {
+                    Duration total = media.getDuration();
+                    pbProgress.setMax(total.toSeconds());
+                }
+            });
+        }
 
     }
 
     public void btnPlayOnAction(ActionEvent e) {
-        
+        if (mediaPlayer != null) {
+            mediaPlayer.play();
+            mediaPlayer.setRate(1.0);
+        }
     }
 
-    public void btnPauseOnAction(ActionEvent actionEvent) {
+    public void btnPauseOnAction(ActionEvent e) {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.pause();
+        }
+    }
+    public void btnStopOnAction(ActionEvent e) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
     }
 
-    public void btnBackwardOnAction(ActionEvent actionEvent) {
+    public void btnSlowRateOnAction(ActionEvent e) {
+        if (mediaPlayer != null) {
+            // Decrease the playback rate (slow down)
+            mediaPlayer.setRate(0.8);
+        }
     }
 
-    public void btnStopOnAction(ActionEvent actionEvent) {
+    public void btnFastForwardOnAction(ActionEvent e) {
+        if (mediaPlayer != null) {
+            // Increase the playback rate (fast forward)
+            mediaPlayer.setRate(1.5);
+        }
     }
 
-    public void btnForwardOnAction(ActionEvent actionEvent) {
+    public void btnBack10sOnAction(ActionEvent e) {
+        // Get the current playback time
+        double currentTime = mediaPlayer.getCurrentTime().toSeconds();
+
+        // Calculate the new time (10 seconds backward)
+        double newTime = currentTime - 10.0;
+
+        // Ensure the new time is within valid bounds (0 to media duration)
+        newTime = Math.max(newTime, 0);
+        newTime = Math.min(newTime, mediaPlayer.getTotalDuration().toSeconds());
+
+        // Seek to the new time
+        mediaPlayer.seek(new javafx.util.Duration(newTime * 1000)); // Convert to milliseconds
     }
 
-    public void btnBack10sOnAction(ActionEvent actionEvent) {
+    public void btnFront10sOnAction(ActionEvent e) {
+        // Get the current playback time
+        double currentTime = mediaPlayer.getCurrentTime().toSeconds();
+
+        // Calculate the new time (10 seconds forward)
+        double newTime = currentTime + 10.0;
+
+        // Ensure the new time is within valid bounds (0 to media duration)
+        newTime = Math.max(newTime, 0);
+        newTime = Math.min(newTime, mediaPlayer.getTotalDuration().toSeconds());
+
+        // Seek to the new time
+        mediaPlayer.seek(new javafx.util.Duration(newTime * 1000)); // Convert to milliseconds
     }
 
-    public void btnFront10sOnAction(ActionEvent actionEvent) {
-    }
+
+
+
 }
